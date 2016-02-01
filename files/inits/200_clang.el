67f49e989a9873cb039760a0494100807770cceb
himaaaatti <sux2mfgj@gmail.com>
2016-02-01 19:37:15 +0900

    change for clang-format in emacs

--- files/inits/200_clang.el
+++ files/inits/200_clang.el
@@ -1,13 +1,31 @@
 ;; -*- tab-width: 4; coding: utf-8; lexical-binding: t -*-
 
+
+(defun clang-format-before-save ()
+    "hook before save"
+    (interactive)
+    (when (eq major-mode 'c-mode) (clang-format-buffer))
+  )
+
 (package-load-with-check
  clang-format
-
  (
-  (with-eval-after-load 'c-mode-hook
 
+  ;; clang-format save hook
+  (add-hook 'before-save-hook 'clang-format-before-save)
+
+  ;; style
+;;  (setq clang-format-style "{BasedOnStyle: Google, IndentWidth: 4}")
+  (setq clang-format-style "file")
+  
+  ;;(with-eval-after-load 'c-mode-hook
+    
     ;;clang-format
-    (add-hook 'before-save-hook 'clang-format-before-save)
-    )
+    ;;(add-hook 'before-save-hook 'clang-format-before-save)
+    ;;)
+
+  ;; auto change mode
+  (add-to-list 'auto-mode-alist '("\\.c\\'" . c-mode))
+  
   )
  )

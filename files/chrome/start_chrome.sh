d08a1415bafdfb07b53d0c3ae050e5a84910751e
himaaaatti <sux2mfgj@gmail.com>
2016-04-08 18:55:20 +0000

    fix script

--- files/chrome/start_chrome.sh
+++ files/chrome/start_chrome.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-inbox=https://inbox.google.com/u/0/
-slack=https://slack.com/
-skype=https://web.skype.com/ja/
-tweetdeck=https://tweetdeck.twitter.com/
+inbox='https://inbox.google.com/u/0/'
+slack='https://slack.com/'
+skype='https://web.skype.com/ja/'
+tweetdeck='https://tweetdeck.twitter.com/'
 
 # line_app_id=menkifleemblimdogmoihpfopnplikde
 
-addresses=${inbox} ${slack} ${skype} ${tweetdeck}
+addresses=(${inbox} ${slack} ${skype} ${tweetdeck})
+echo ${addresses}
 
 # google-chrome-stable --app-id=${line_app_id} > /dev/null 2>&1 &
-google-chrome-stable ${addresses} > /dev/null 2>&1 
+google-chrome-stable ${addresses[@]} > /dev/null 2>&1 
 
